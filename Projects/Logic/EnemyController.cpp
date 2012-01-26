d9b6036d9e233285183edf9a6b8dcf63dc5c509c
Luis Mendoza <luis_mencem@hotmail.com>
2012-01-26 14:21:10 +0000

    El anterior estaba incompleto. Creo que ahora está todo. Muñeco que se mueve de punto aleatorio en punto aleatorio del escenario en línea recta. No tiene en cuenta ningún tipo de obstáculo, simplemente lo atraviesa. Todavía tengo que arreglar muchas cosas, pero de momento es el primer acercamiento a un enemigo que se mueve por el mapa.
    git-svn-id: http://mastervj.fdi.ucm.es/svn/master/grupo3/trunk@119 afcbbe0f-131b-4dd2-a684-3ccf32d4d6de

---
+++ Projects/Logic/EnemyController.cpp
@@ -0,0 +1,104 @@
+/**
+@file EnemyController.cpp
+
+Contiene la implementación del componente que controla el movimiento 
+del enemigo.
+ 
+@see Logic::CEnemyController
+@see Logic::IComponent
+
+@author Luis Mendoza
+@date Enero, 2012
+*/
+
+#include "EnemyController.h"
+
+#include "Logic/Entity/Entity.h"
+#include "Logic/Maps/Map.h"
+#include "Map/MapEntity.h"
+
+#include "Graphics/Server.h"
+#include "Graphics/Scene.h"
+
+// Includes para pruebas de paso de mensajes a componentes de IA
+#include "AI/Server.h"
+#include "AI/Movement.h"
+
+
+namespace Logic
+{
+	IMP_FACTORY(CEnemyController);
+	
+	//---------------------------------------------------------
+
+	bool CEnemyController::spawn(CEntity *entity, CMap *map, const Map::CEntity *entityInfo) 
+	{
+		if(!IComponent::spawn(entity,map,entityInfo))
+			return false;
+		
+		if(entityInfo->hasAttribute("speed"))
+			_speed = entityInfo->getFloatAttribute("speed");
+
+		if(entityInfo->hasAttribute("position"))
+			_destino = entityInfo->getFloatAttribute("position");
+
+		_moving = false;
+
+		return true;
+
+	} // spawn
+	
+	//---------------------------------------------------------
+
+	bool CEnemyController::activate()
+	{
+		return true;
+	} // activate
+	
+	//---------------------------------------------------------
+
+	void CEnemyController::deactivate()
+	{
+
+	} // deactivate
+	
+	//---------------------------------------------------------
+
+	bool CEnemyController::accept(const TMessage &message)
+	{
+		return false;
+
+	} // accept
+	
+	//---------------------------------------------------------
+
+	void CEnemyController::process(const TMessage &message)
+	{
+
+	} // process
+	
+	//---------------------------------------------------------
+
+	void CEnemyController::tick(unsigned int msecs)
+	{
+		IComponent::tick(msecs);
+		
+		// Si no se está moviendo calculamos un nuevo punto de destino y el vector _orientation
+		// de desplazamiento en cada tick para llegar a _destino
+		if (!_moving)
+		{
+			_destino.x = rand() % 200 - 100;
+			_destino.y = 0.0f;
+			_destino.z = rand() % 200 - 100;
+			_orientation = _destino - _entity->getPosition();
+			_orientation.normalise();
+			_orientation *= msecs * _speed;
+		}
+
+		_entity->setPosition(_entity->getPosition() + _orientation);
+		_moving = (_entity->getPosition() - _destino).length() >= 1;
+
+	} // tick
+
+} // namespace Logic
+
