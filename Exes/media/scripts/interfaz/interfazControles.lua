7cec156b7ecbae6c0fbaae6aa9674fcc12e288e8
Isaac Gallarzagoitia <isgallar@gmail.com>
2012-05-29 17:28:38 +0000

    Carga bien el boton con la imagen y la funcion
    git-svn-id: http://mastervj.fdi.ucm.es/svn/master/grupo3/trunk@3052 afcbbe0f-131b-4dd2-a684-3ccf32d4d6de

--- Exes/media/scripts/interfaz/interfazControles.lua
+++ Exes/media/scripts/interfaz/interfazControles.lua
@@ -1,14 +1,13 @@
 function fsadfsadf()
-	print("4")
+	print("fsadfsadf")
 end
 
 function funcionConstruir()
-	print("1")
-	cargarBoton(1,"torreta","fsadfsadf")
-	--cargarBoton(2,"torreta","inicializarBotonesDia")
-	--cargarBoton(3,"torreta","inicializarBotonesDia")
-	--cargarBoton(4,"volver","inicializarBotonesDia")
-	print("3")
+	print("funcionConstruir")
+	cargarBoton(1,"torreta","inicializarBotonesDia")
+	cargarBoton(2,"torreta","inicializarBotonesDia")
+	cargarBoton(3,"torreta","inicializarBotonesDia")
+	cargarBoton(4,"volver","inicializarBotonesDia")
 end
 
 function inicializarBotonesDia()
@@ -19,22 +18,41 @@ function inicializarBotonesDia()
 	cargarBoton(4,"jeringa","funcionConstruir")
 end
 
+
+--Las funciones de los botones redirigen a la funcion que asignemos a dicho boton
+--No asignamos directamente las funciones al evento del boton, porque da problemas al cambiar el mismo evento que se esta produciendo
+function clickBoton1()
+	_G[funcionBoton1]()
+end
+function clickBoton2()
+	_G[funcionBoton2]()
+end
+function clickBoton3()
+	_G[funcionBoton3]()
+end
+function clickBoton4()
+	_G[funcionBoton4]()
+end
+
+--Funcion que cargar en un boton, una imagen y una funcion
 function cargarBoton(numBoton,imageName,funcion)
 	--Guardamos en la variable local "boton" el boton que vamos a modificar
 	local boton
 	if numBoton == 1 then
 		boton = botonControles1
+		print("asignada funcion1 a: ")
+		print(funcion)
+		funcionBoton1 = funcion
     elseif numBoton == 2 then
 		boton = botonControles2
+		funcionBoton2 = funcion
     elseif numBoton == 3 then
 		boton = botonControles3
+		funcionBoton3 = funcion
 	elseif numBoton == 4 then
 		boton = botonControles4
+		funcionBoton4 = funcion
     end
-
-	--Asignamos la funcion al boton
-	boton:removeAllEvents(void)
-	boton:subscribeEvent("Clicked", funcion)
 	
 	--Cargamos las imagenes al boton
 	local imageNameComun = "set:InterfazUtils image:"
@@ -45,9 +63,9 @@ function cargarBoton(numBoton,imageName,funcion)
 	boton:setProperty("HoverImage",imageNameFinal)
 	imageNameFinal = imageNameComun .. imageName .. 3
 	boton:setProperty("PushedImage",imageNameFinal)
-	print("2")
 end
 
+--Inicializacion de la interfaz de los controles
 function inicializarInterfazControles()
 	-- Cargamos la interfaz
 	CEGUI.WindowManager:getSingleton():loadWindowLayout("InterfazControles.layout")
@@ -63,6 +81,12 @@ function inicializarInterfazControles()
 	botonControles2 = CEGUI.WindowManager:getSingleton():getWindow("InterfazControles/Menu/b2")
 	botonControles3 = CEGUI.WindowManager:getSingleton():getWindow("InterfazControles/Menu/b3")
 	botonControles4 = CEGUI.WindowManager:getSingleton():getWindow("InterfazControles/Menu/b4")
+	
+	--Subscribimos los botones a sus funciones
+	botonControles1:subscribeEvent("Clicked", clickBoton1)
+	botonControles2:subscribeEvent("Clicked", clickBoton2)
+	botonControles3:subscribeEvent("Clicked", clickBoton3)
+	botonControles4:subscribeEvent("Clicked", clickBoton4)
 	
 	--Inicializamos los botones del dia
 	inicializarBotonesDia()
